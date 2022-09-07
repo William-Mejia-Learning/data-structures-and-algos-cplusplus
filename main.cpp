a3adb5800fdd6401c54255962c09aeef8d9503d4
williamxmejia <willx1995@gmail.com>
2022-09-07 08:26:52 -0500

    arrays & functions - Pass byt reference

--- main.cpp
+++ main.cpp
@@ -2,28 +2,53 @@
 
 using namespace std;
 
+void printArray(int * arr, int n) {
+    cout << "In Function " << sizeof(arr) << endl;
+    arr[0] = 100;
+
+//    n = sizeof(arr) / sizeof(int);
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << endl;
+    }
+}
+
 int main() {
 
-    int marks[100] = {0};
+//    int marks[100] = {0};
+//
+//    int n;
+//    cout << "Enter the no of students ";
+//
+//    cin >> n;
+//
+//    for(int i=0; i<n; i++){
+//        cin >> marks[i];
+//
+//        marks[i] = marks[i]*2;
+//    }
+//
+//    for(int i=0; i<100; i++){
+//        cout << marks[i] << " ,";
+//    }
 
-    int n;
-    cout << "Enter the no of students ";
-
-    cin >> n;
-
-    for(int i=0; i<n; i++){
-        cin >> marks[i];
-
-        marks[i] = marks[i]*2;
-    }
-
-    for(int i=0; i<100; i++){
-        cout << marks[i] << " ,";
-    }
-
-    cout << endl;
+//    cout << endl;
 
     // There is no garbage collection in C++
+
+    int arr[] = {1, 2, 3, 4, 5, 6};
+
+    int n = sizeof(arr) / sizeof(int);
+
+    cout << "In Main " << sizeof(arr) << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << endl;
+    }
+
+    printArray(arr, n);
+
+
 
     return 0;
 }
