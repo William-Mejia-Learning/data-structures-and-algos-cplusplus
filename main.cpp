a248126798e4ea371f2f1590812f87feb563ed1f
williamxmejia <willx1995@gmail.com>
2022-09-07 10:58:35 -0500

    Array Reverse

--- main.cpp
+++ main.cpp
@@ -2,44 +2,38 @@
 
 using namespace std;
 
-int binary_search(int arr[], int n, int key) {
-    // Implement binary search: time complexity O(log N)
-    // Fast and efficient
-    // Search space is monotonic
+int reverseArray(int arr[], int n) {
+
     int s = 0;
-    int e = n - 1;
+    int e = n -1;
 
-    while(s<=e) {
-        int mid = (s+e)/2;
-
-        if(arr[mid] == key){
-            return mid;
-        }
-        else if (arr[mid] > key){
-            e = mid - 1;
-        } else {
-            s = mid + 1;
-        }
+    // O(N)
+    while(s < e){
+        swap(arr[s],arr[e]);
+        s += 1;
+        e -= 1;
     }
-    return -1;
+
 }
 
 int main() {
 
-    int arr[] = {10,20,30,40,45, 60, 70,89};
+    int arr[] = {10,20,30,40,45, 60, 90,90};
     int n = sizeof(arr) / sizeof(int);
 
-    int key;
-    cin >> key;
-
-    int index = binary_search(arr, n, key);
-
-    if(index != -1){
-        cout << key << " is present at index " << index << endl;
+    //print the output
+    for(int i=0; i < n; i++){
+        cout<< arr[i] << " ";
     }
-    else {
-        cout << key << " is NOT Found!" << endl;
+    cout << endl;
+
+    reverseArray(arr, n);
+
+    //print the output
+    for(int i=0; i < n; i++){
+        cout<< arr[i] << " ";
     }
+    cout << endl;
 
     return 0;
 }
