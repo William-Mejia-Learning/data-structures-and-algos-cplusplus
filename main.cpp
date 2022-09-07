73956485a9a9d8753b174bfb5d7f99b45e14e10a
williamxmejia <willx1995@gmail.com>
2022-09-07 08:02:52 -0500

    Arrays- input, output, update

--- main.cpp
+++ main.cpp
@@ -4,22 +4,26 @@ using namespace std;
 
 int main() {
 
-    int marks[100];
+    int marks[100] = {0};
 
     int n;
     cout << "Enter the no of students ";
 
     cin >> n;
 
-    for(int i=0; i< n; i++){
+    for(int i=0; i<n; i++){
         cin >> marks[i];
+
+        marks[i] = marks[i]*2;
     }
 
-    for(int i=0; i<n; i++){
+    for(int i=0; i<100; i++){
         cout << marks[i] << " ,";
     }
 
     cout << endl;
+
+    // There is no garbage collection in C++
 
     return 0;
 }
