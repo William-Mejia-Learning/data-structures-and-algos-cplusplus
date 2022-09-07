43169daef7c928e8246dc6b75fa9c74db736f410
William Mejia <willx1995@gmail.com>
2022-09-07 14:11:08 -0500

    Subarray Sum - I (Brute Force)

--- main.cpp
+++ main.cpp
@@ -2,35 +2,41 @@
 
 using namespace std;
 
-int printSubArrays(int arr[], int n) {
+int largestSubarraySum1(int arr[], int n) {
+
+    int largest_sum = 0;
 
     //Time O(n(N^3)
-    int sum;
 
     for(int i = 0; i<n; i++){
         for(int j=i;j<n;j++){
             //cout << "(" << i << ", " << j <<"),";
-            for(int k = i; k<=j; k++){
-                cout << arr[k] << ", ";
-            }
-            cout << endl;
-        }
-        cout << endl;
-    }
 
+            int subarraySum = 0;
+            for(int k = i; k<=j; k++){
+//                cout << arr[k] << ", ";
+                subarraySum += arr[k];
+            }
+
+            //put a check is subarraySum > largest_sum
+            largest_sum = max(largest_sum, subarraySum);
+        }
+    }
+    return largest_sum;
 }
 
-//Print the sum of each subarray & finding out the largest sum
+// find out the largest sum of subarray
+// Brute Force
 
 int main() {
 
-    int arr[] = {10,20,30,40,50, 60};
+    int arr[] = {-2,3,4,-1,5,-12,6,1,3};
     int n = sizeof(arr) / sizeof(int);
 
 
     // Print all the elements
 
-    printSubArrays(arr, n);
+    cout << largestSubarraySum1(arr, n) << endl;
 
     return 0;
 }
