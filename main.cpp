f1c528473b01e1075cf642e81e8722c24d163cff
williamxmejia <willx1995@gmail.com>
2022-09-07 20:00:45 -0500

    subarray sum - II (Prefix Sums)

--- main.cpp
+++ main.cpp
@@ -2,27 +2,27 @@
 
 using namespace std;
 
+// Prefix Sum Approach O(N^2)
 int largestSubarraySum1(int arr[], int n) {
+    //Prefix sums
+    int prefix[n] = {0};
+    prefix[0] = arr[0];
+    for (int i = 1; i < n; i++) {
+        prefix[i] = prefix[i - 1] + arr[i];
+    }
 
+    // largest sum login
     int largest_sum = 0;
 
-    //Time O(n(N^3)
-
-    for(int i = 0; i<n; i++){
-        for(int j=i;j<n;j++){
-            //cout << "(" << i << ", " << j <<"),";
-
-            int subarraySum = 0;
-            for(int k = i; k<=j; k++){
-//                cout << arr[k] << ", ";
-                subarraySum += arr[k];
-            }
-
-            //put a check is subarraySum > largest_sum
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            int subarraySum = i > 0 ? prefix[j] - prefix[i - 1] : prefix[j];
             largest_sum = max(largest_sum, subarraySum);
         }
     }
+
     return largest_sum;
+
 }
 
 // find out the largest sum of subarray
@@ -30,7 +30,7 @@ int largestSubarraySum1(int arr[], int n) {
 
 int main() {
 
-    int arr[] = {-2,3,4,-1,5,-12,6,1,3};
+    int arr[] = {-2, 3, 4, -1, 5, -12, 6, 1, 3};
     int n = sizeof(arr) / sizeof(int);
 
 
